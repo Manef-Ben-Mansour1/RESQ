04c81432f1149b20b986e3075d3a779f6da7e64d
Manef-Ben-Mansour1 <mohamedmanef.benmansour@insat.ucar.tn>
2023-11-13 22:40:38 +0100

    adding second page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import  React from 'react';
+import  React,{useState} from 'react';
 import './App.css';
 import FrontPage from "./Components/Front Page";
 import EmergencyCall from "./Components/Emergency Call";
 
+
 function App() {
+    const [state,setState]=useState(0)
   return (
     <>
-         <EmergencyCall />
+        {state===0 ? <FrontPage setState={setState}></FrontPage> :<EmergencyCall setState={setState} />}
+
 
 
     </>
